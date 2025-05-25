1f290cfe2e3d541f5131fe1bd8f86694965b5f41
HakimaNoori <hakima.noori.1379@gmail.com>
2025-05-25 12:12:32 +0430

    fix(backend --> setup): remove the cors package

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const proverbRoutes = require("./routes/ProverbRoutes.js");
-
-app.use(
-  cors({
-    origin: "http://localhost:5174",
-  })
-);
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
--- frontend/src/components/proverbsList.jsx
+++ frontend/src/components/proverbsList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
 
 const ProverbList = () => {
   const [proverbs, setProverbs] = useState([]);
