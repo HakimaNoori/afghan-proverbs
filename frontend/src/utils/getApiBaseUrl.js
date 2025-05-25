2acc6ab6c9fe461911d0bfdf4d1cb5f36aadb3f1
HakimaNoori <hakima.noori.1379@gmail.com>
2025-05-25 15:47:51 +0430

    fix(frontend --> getApi): remove the log

--- frontend/src/utils/getApiBaseUrl.js
+++ frontend/src/utils/getApiBaseUrl.js
@@ -3,6 +3,5 @@ export const getApiBaseUrl = () => {
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  console.log("Backend URL:", backendUrl);
   return isLocal ? "" : backendUrl;
 };
